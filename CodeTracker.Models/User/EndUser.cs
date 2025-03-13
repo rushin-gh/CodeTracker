cde82ba3a3cf6e6e3aaaac0aad38fa3b49563ec9
Rushi <rushin.mailme@gmail.com>
2025-03-14 02:08:00 +0530

    Initial Project setup

---
+++ CodeTracker.Models/User/EndUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTracker.Models.User
+{
+    public class EndUser
+    {
+        [DisplayName("UserID")]
+        public int EndUserUserid {  get; set; }
+
+        [DisplayName("Username")]
+        public string EndUserUsername { get; set; }
+
+        [DisplayName("Password")]
+        public string EndUserPassword { get; set; }
+
+        public EndUser()
+        {
+            EndUserUserid = 0;
+            EndUserUsername = string.Empty;
+            EndUserPassword = string.Empty;
+        }
+
+        public EndUser(string endUserUsername, string endUserPassword)
+        {
+            EndUserUsername = endUserUsername;
+            EndUserPassword = endUserPassword;
+        }
+
+        public EndUser(int endUserId, string endUserUsername, string endUserPassword) 
+            : this(endUserUsername, endUserPassword)
+        {
+            EndUserUserid = endUserId;
+        }
+    }
+}
---
+++ WebApp/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using CodeTracker.Models.User;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            EndUser endUser = new EndUser();
+            return View(endUser);
+        }
+    }
+}
